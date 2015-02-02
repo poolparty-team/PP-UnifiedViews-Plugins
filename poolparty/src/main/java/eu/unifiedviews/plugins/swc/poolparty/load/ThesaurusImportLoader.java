0a44ffc3b23e858883814213a7953cc25dcb3db4
Christian Mader <mail@christianmader.net>
2015-02-02 14:12:51 +0100

    implementation of importloader

--- poolparty/src/main/java/eu/unifiedviews/plugins/swc/poolparty/load/ThesaurusImportLoader.java
+++ poolparty/src/main/java/eu/unifiedviews/plugins/swc/poolparty/load/ThesaurusImportLoader.java
@@ -1,19 +1,93 @@
 package eu.unifiedviews.plugins.swc.poolparty.load;
 
+import eu.unifiedviews.dataunit.DataUnit;
+import eu.unifiedviews.dataunit.DataUnitException;
+import eu.unifiedviews.dataunit.rdf.RDFDataUnit;
 import eu.unifiedviews.dpu.DPU;
 import eu.unifiedviews.dpu.DPUContext;
 import eu.unifiedviews.dpu.DPUException;
 import eu.unifiedviews.helpers.dpu.config.AbstractConfigDialog;
 import eu.unifiedviews.helpers.dpu.config.ConfigDialogProvider;
 import eu.unifiedviews.helpers.dpu.config.ConfigurableBase;
+import eu.unifiedviews.plugins.swc.poolparty.PoolPartyApiConfig;
+import eu.unifiedviews.plugins.swc.poolparty.api.PptApiConnector;
+import org.openrdf.model.Statement;
+import org.openrdf.model.impl.ContextStatementImpl;
+import org.openrdf.model.impl.URIImpl;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFHandler;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.Rio;
+import org.openrdf.rio.helpers.RDFHandlerWrapper;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 @DPU.AsLoader
 public class ThesaurusImportLoader extends
         ConfigurableBase<ThesaurusImportConfig> implements
         ConfigDialogProvider<ThesaurusImportConfig>
 {
-    public ThesaurusImportLoader(Class<ThesaurusImportConfig> configClass) {
-        super(configClass);
+
+    @DataUnit.AsInput(name = "input")
+    public RDFDataUnit inputDataUnit;
+
+    public ThesaurusImportLoader() {
+        super(ThesaurusImportConfig.class);
+    }
+
+    @Override
+    public void execute(DPUContext context) throws DPUException, InterruptedException {
+        try {
+            importRdf();
+            createSnapshot();
+        }
+        catch (Exception e) {
+            throw new DPUException("Error loading data", e);
+        }
+    }
+
+    private void importRdf() throws IOException, DataUnitException, RDFHandlerException, RepositoryException {
+        PoolPartyApiConfig poolPartyApiConfig = config.getApiConfig();
+        URL url = PptApiConnector.getServiceUrl(poolPartyApiConfig.getServer(),
+                "PoolParty/api/thesaurus/" +config.getApiConfig().getProjectId()+ "/import");
+
+        URLConnection con = url.openConnection();
+        con.setDoOutput(true);
+        con.setRequestProperty("Content-Type", RDFFormat.TRIG.getDefaultMIMEType());
+
+        RDFHandler handler = Rio.createWriter(RDFFormat.TRIG, con.getOutputStream());
+        final String targetGraph = config.getGraph();
+        if (targetGraph != null) {
+            handler = new RDFHandlerWrapper(handler) {
+                @Override
+                public void handleStatement(Statement st) throws RDFHandlerException {
+                    super.handleStatement(new ContextStatementImpl(st.getSubject(),
+                            st.getPredicate(),
+                            st.getObject(),
+                            new URIImpl(targetGraph)));
+                }
+            };
+        }
+        inputDataUnit.getConnection().export(handler);
+        con.getOutputStream().close();
+
+        poolPartyApiConfig.getAuthentication().visit(con);
+    }
+
+    private void createSnapshot() throws IOException, DPUException {
+        PoolPartyApiConfig poolPartyApiConfig = config.getApiConfig();
+        URL url = PptApiConnector.getServiceUrl(poolPartyApiConfig.getServer(),
+                "PoolParty/api/thesaurus/" +config.getApiConfig().getProjectId()+ "/snapshot");
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        if (con.getResponseCode() != 200) {
+            throw new DPUException("PPT API returned response code: " + con.getResponseCode());
+        }
+        con.disconnect();
     }
 
     @Override
@@ -21,8 +95,4 @@ public class ThesaurusImportLoader extends
         return new ThesaurusImportDialog();
     }
 
-    @Override
-    public void execute(DPUContext context) throws DPUException, InterruptedException {
-
-    }
 }
---
+++ poolparty/src/test/java/eu/unifiedviews/plugins/swc/poolparty/load/ThesaurusImportLoaderTest.java
@@ -0,0 +1,18 @@
+package eu.unifiedviews.plugins.swc.poolparty.load;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ThesaurusImportLoaderTest {
+
+    @Test
+    public void importIntoDefaultGraph() {
+        Assert.fail();
+    }
+
+    @Test
+    public void importIntoNamedGraph() {
+        Assert.fail();
+    }
+
+}
