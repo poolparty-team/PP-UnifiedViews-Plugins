87396cbb56a5efc4c9395e7f08e191958f7b4809
Yang Yuanzhe <yyz1989@hotmail.com>
2015-11-23 19:00:58 +0100

    Added unit test and test resources for file extraction

--- uv-t-poolpartyConceptExtractor/src/test/java/eu/unifiedviews/plugins/swc/transformer/conceptextractor/FileExtractionTest.java
+++ uv-t-poolpartyConceptExtractor/src/test/java/eu/unifiedviews/plugins/swc/transformer/conceptextractor/FileExtractionTest.java
@@ -1,9 +1,12 @@
 package eu.unifiedviews.plugins.swc.transformer.conceptextractor;
 
 import cz.cuni.mff.xrg.odcs.dpu.test.TestEnvironment;
+import eu.unifiedviews.dataunit.files.FilesDataUnit;
+import eu.unifiedviews.dataunit.files.WritableFilesDataUnit;
 import eu.unifiedviews.dataunit.rdf.WritableRDFDataUnit;
 import eu.unifiedviews.helpers.dataunit.rdf.RDFHelper;
 import eu.unifiedviews.helpers.dpu.test.config.ConfigurationBuilder;
+import org.apache.commons.io.IOUtils;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -15,19 +18,22 @@ import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.turtle.TurtleWriter;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 /**
- * Created by yyz on 15/10/15.
+ * Created by yyz on 23/11/15.
  */
-public class DpuTest {
+public class FileExtractionTest {
     private static ConceptExtractor extractor;
     private static TestEnvironment env;
     private static WritableRDFDataUnit output;
     private static WritableRDFDataUnit input;
+    private static WritableFilesDataUnit fileInput;
     private static RepositoryConnection connection;
     private static Properties properties;
     private static ConceptExtractorConfig_V1 config;
@@ -36,10 +42,12 @@ public class DpuTest {
     public static void before() throws Exception {
         extractor = new ConceptExtractor();
         env = new TestEnvironment();
+
         input = env.createRdfInput("input", false);
+        fileInput = env.createFilesInput("fileInput");
         output = env.createRdfOutput("output", false);
 
-        InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.ttl");
+        InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("file.ttl");
 
         connection = input.getConnection();
         URI graph = input.addNewDataGraph("test");
@@ -48,6 +56,16 @@ public class DpuTest {
         connection.export(new TurtleWriter(inputBos), graph);
 
         Assert.assertTrue(connection.size(graph) > 0);
+
+        File inputFile = new File(java.net.URI.create(fileInput.addNewFile("file1.txt")));
+        try (FileOutputStream fout = new FileOutputStream(inputFile)) {
+            IOUtils.copy(Thread.currentThread().getContextClassLoader().getResourceAsStream("file1.txt"), fout);
+        }
+
+        inputFile = new File(java.net.URI.create(fileInput.addNewFile("file2.txt")));
+        try (FileOutputStream fout = new FileOutputStream(inputFile)) {
+            IOUtils.copy(Thread.currentThread().getContextClassLoader().getResourceAsStream("file2.txt"), fout);
+        }
 
         properties = new Properties();
         properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("target.properties"));
